de8edbee2fe16197d8888e41b32eb48572e3f8d2
ashutosh chaudhary <ashuchaudhary6969@gmail.com>
2024-12-20 20:34:13 +0530

    asas

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 import express from "express";
 import { scrap } from "./scrap.js";
 import { routes } from "./routes.js";
-
+import bodyParser from "body-parser";
+import cors from "cors";
 const app = express();
 const PORT = 5000;
 
-app.use(express.json());
 
-app.get("/scrap", scrap);
+const corsOptions = {
+  origin: '*',
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
